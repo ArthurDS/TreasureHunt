fe5496c5f80153d3d2fe88a4936cf32f72dee7d8
LarsdeGraaff <lars.dg@gmail.com>
2016-04-12 12:06:26 +0200

    n

--- TreasureHunt/TreasureHunt/LocationDetailTableViewController.swift
+++ TreasureHunt/TreasureHunt/LocationDetailTableViewController.swift
@@ -14,7 +14,7 @@ class LocationDetailTableViewController: UITableViewController {
     var location: Location!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+       // self.title = locationManager.description
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
