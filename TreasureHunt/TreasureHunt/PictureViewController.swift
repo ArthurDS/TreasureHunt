ace0410fb504defef5da3b422f1d38d7def218c0
Arthur Despiegelaere <arthurdespiegelaere@gmail.com>
2016-04-12 14:07:35 +0200

    fefe

--- TreasureHunt/TreasureHunt/PictureViewController.swift
+++ TreasureHunt/TreasureHunt/PictureViewController.swift
@@ -7,49 +7,111 @@
 //
 
 import UIKit
-import MobileCoreServices
 
-class PictureViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class PictureViewController: UIViewController {
     
     
-    @IBOutlet weak var PictureImageView: UIImageView!
-    var newMedia: Bool?
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        
-        
-    }
-    
-    @IBAction func useCamera(sender: AnyObject) {
-        
-        if UIImagePickerController.isSourceTypeAvailable(
-            UIImagePickerControllerSourceType.Camera) {
-            
-            let imagePicker = UIImagePickerController()
-            
-            imagePicker.delegate = self
-            imagePicker.sourceType =
-                UIImagePickerControllerSourceType.Camera
-            imagePicker.mediaTypes = [kUTTypeImage as NSString as String]
-            imagePicker.allowsEditing = false
-            
-            self.presentViewController(imagePicker, animated: true,
-                                       completion: nil)
-            newMedia = true
-        }
-        
-    }
+//    @IBOutlet weak var PictureImageView: UIImageView!
+//    var newMedia: Bool?
+//    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        
+//        
+//        
+//    }
+//    
+//    @IBAction func cameraRoll(sender: AnyObject) {
+//        
+//        if UIImagePickerController.isSourceTypeAvailable(
+//            UIImagePickerControllerSourceType.SavedPhotosAlbum) {
+//            let imagePicker = UIImagePickerController()
+//            
+//            imagePicker.delegate = self
+//            imagePicker.sourceType =
+//                UIImagePickerControllerSourceType.PhotoLibrary
+//            imagePicker.mediaTypes = [kUTTypeImage as NSString as String]
+//            imagePicker.allowsEditing = false
+//            self.presentViewController(imagePicker, animated: true,
+//                                       completion: nil)
+//            newMedia = false
+//        }
+//    
+//    }
+//    
+//    @IBAction func useCamera(sender: AnyObject) {
+//        
+//        if UIImagePickerController.isSourceTypeAvailable(
+//            UIImagePickerControllerSourceType.Camera) {
+//            
+//            let imagePicker = UIImagePickerController()
+//            
+//            imagePicker.delegate = self
+//            imagePicker.sourceType =
+//                UIImagePickerControllerSourceType.Camera
+//            imagePicker.mediaTypes = [kUTTypeImage as NSString as String]
+//            imagePicker.allowsEditing = false
+//            
+//            self.presentViewController(imagePicker, animated: true,
+//                                       completion: nil)
+//            newMedia = true
+//        }
+//        
+//    }
+//    
+//    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]) {
+//        
+//        let mediaType = info[UIImagePickerControllerMediaType] as! String
+//        
+//        self.dismissViewControllerAnimated(true, completion: nil)
+//        
+//        if mediaType.isEqualToString(kUTTypeImage as! String) {
+//            let image = info[UIImagePickerControllerOriginalImage]
+//                as! UIImage
+//            
+//            imageView.image = image
+//            
+//            if (newMedia == true) {
+//                UIImageWriteToSavedPhotosAlbum(image, self,
+//                                               "image:didFinishSavingWithError:contextInfo:", nil)
+//            } else if mediaType.isEqualToString(kUTTypeMovie as! String) {
+//                // Code to support video here
+//            }
+//            
+//        }
+//    }
+//    
+//    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+//        let mediaType = info[UIImagePickerControllerMediaType] as! String
+//        
+//        self.dismissViewControllerAnimated(true, completion: nil)
+//        
+////        if mediaType.isEqualToString(kUTTypeImage as! String) {
+////            let image = info[UIImagePickerControllerOriginalImage]
+////                as! UIImage
+//        
+//        if mediaType.i
+//        
+//            imageView.image = image
+//            
+//            if (newMedia == true) {
+//                UIImageWriteToSavedPhotosAlbum(image, self,
+//                                               "image:didFinishSavingWithError:contextInfo:", nil)
+//            } else if mediaType.isEqualToString(kUTTypeMovie as! String) {
+//                // Code to support video here
+//            }
+//            
+//        }
+//    }
 }
 
 /*
- // MARK: - Navigation
+  MARK: - Navigation
  
- // In a storyboard-based application, you will often want to do a little preparation before navigation
+  In a storyboard-based application, you will often want to do a little preparation before navigation
  override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
- // Get the new view controller using segue.destinationViewController.
- // Pass the selected object to the new view controller.
+  Get the new view controller using segue.destinationViewController.
+  Pass the selected object to the new view controller.
  }
  */
 
