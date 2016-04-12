77946ea339c30c2eb6d7af9ecce3a4cec22993fa
Arthur Despiegelaere <arthurdespiegelaere@gmail.com>
2016-04-12 10:01:38 +0200

    koko

--- TreasureHunt/TreasureHunt/LocationManager.swift
+++ TreasureHunt/TreasureHunt/LocationManager.swift
@@ -7,12 +7,13 @@
 //
 
 import UIKit
+import CoreData
 
 
-class LocationManager: NSObject {
+class LocationManager: NSManagedObject {
     
 static let sharedManager = LocationManager()
-    
-    
 
+    
+    
 }
