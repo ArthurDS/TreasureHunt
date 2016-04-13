e7a4227e08d589fa895861dd6ff1c2b34b9e5c29
Kristof Renotte <renottekristof@gmail.com>
2016-04-13 12:02:30 +0200

    fgsdg

--- TreasureHunt/TreasureHunt/LocationDetailTableViewController.swift
+++ TreasureHunt/TreasureHunt/LocationDetailTableViewController.swift
@@ -12,19 +12,19 @@ import CoreLocation
 import CoreData
 
 class LocationDetailTableViewController: UITableViewController, CLLocationManagerDelegate, MKMapViewDelegate ,NSFetchedResultsControllerDelegate{
-
+    
     
     let locationManager = LocationManager.sharedManager
     var location: Location!
     let context = (UIApplication.sharedApplication().delegate as! AppDelegate).managedObjectContext
     var isInitialized = false
-
+    
     @IBOutlet weak var mapView: MKMapView!
     
     
     
-   
-
+    
+    
     
     var mapLocationManager: CLLocationManager!
     var myLocations: [CLLocation] = []
@@ -40,12 +40,12 @@ class LocationDetailTableViewController: UITableViewController, CLLocationManage
             mapLocationManager.requestAlwaysAuthorization()
             mapLocationManager.startUpdatingLocation()
             setAnotation()
- 
+            
             mapView.showsUserLocation = true
         }
         
-    
-               
+        
+        
         
     }
     
@@ -62,14 +62,14 @@ class LocationDetailTableViewController: UITableViewController, CLLocationManage
             let span:MKCoordinateSpan = MKCoordinateSpanMake(latDelta, lonDelta)
             let location: CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
             let region: MKCoordinateRegion = MKCoordinateRegionMake(location, span)
-
+            
             self.mapView.setRegion(region, animated: true)
             self.mapView.showsUserLocation = true
         }
     }
     
     func setAnotation() {
-
+        
         
         let locManager = CLLocationManager()
         locManager.requestWhenInUseAuthorization()
@@ -78,134 +78,127 @@ class LocationDetailTableViewController: UITableViewController, CLLocationManage
         print(currentLocation)
         
         let location:CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: 50.881581, longitude: 4.711865)
-
-
+        
+        
         
         let anotation = MKPointAnnotation()
         anotation.coordinate = location
         anotation.title = "Kristof Renotte"
         anotation.subtitle = "op 20 km van uw locatie"
-    
+        
         mapView.addAnnotation(anotation)
-
+        
         
         
     }
     
- 
-
-
-  
+    
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 1
     }
-
+    
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return 3
     }
     
 
-//    func mapView(mapView: MKMapView,
-//                 viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
-//
-//        
-//        if annotation.isKindOfClass(MKUserLocation) {
-//            return nil
-//        }
-//        
-//        let detailButton: UIButton = UIButton(type: UIButtonType.DetailDisclosure)
-//        
-//        // Reuse the annotation if possible
-//        var annotationView = mapView.dequeueReusableAnnotationViewWithIdentifier("pin")
-//        
-//        if annotationView == nil
-//        {
-//            annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: "pin")
-//            annotationView!.canShowCallout = true
-//            annotationView!.image = UIImage(named: "icon")
-//            annotationView!.rightCalloutAccessoryView = detailButton
-//        }
-//        else
-//        {
-//            annotationView!.annotation = annotation
-//        }
-//        
-//        return annotationView
-//    }
+    func mapView(mapView: MKMapView,
+                 viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
+
+        
+        if annotation.isKindOfClass(MKUserLocation) {
+            return nil
+        }
+        
+        let detailButton: UIButton = UIButton(type: UIButtonType.DetailDisclosure)
+        
+        // Reuse the annotation if possible
+        var annotationView = mapView.dequeueReusableAnnotationViewWithIdentifier("pin")
+        
+        if annotationView == nil
+        {
+            annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: "pin")
+            annotationView!.canShowCallout = true
+            annotationView!.image = UIImage(named: "annotation_pin")
+            annotationView!.rightCalloutAccessoryView = detailButton
+        }
+        else
+        {
+            annotationView!.annotation = annotation
+        }
+        
+        return annotationView
+    }
 
 
 
 
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell  {
         let cell = tableView.dequeueReusableCellWithIdentifier("detailCell", forIndexPath: indexPath) as! DescriptionTableViewCell
-      
+        
         // Configure the cell...
-
+        
         return cell
     }
- 
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-}
-
-extension CLLocationCoordinate2D {
     
-    func distanceInMetersFrom(otherCoord : CLLocationCoordinate2D) -> CLLocationDistance {
-        let firstLoc = CLLocation(latitude: self.latitude, longitude: self.longitude)
-        let secondLoc = CLLocation(latitude: otherCoord.latitude, longitude: otherCoord.longitude)
-        return firstLoc.distanceFromLocation(secondLoc)
-    }
+    
+    /*
+     // Override to support conditional editing of the table view.
+     override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+     // Return false if you do not want the specified item to be editable.
+     return true
+     }
+     */
+    
+    /*
+     // Override to support editing the table view.
+     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+     if editingStyle == .Delete {
+     // Delete the row from the data source
+     tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+     } else if editingStyle == .Insert {
+     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+     }
+     }
+     */
+    
+    /*
+     // Override to support rearranging the table view.
+     override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+     
+     }
+     */
+    
+    /*
+     // Override to support conditional rearranging of the table view.
+     override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+     // Return false if you do not want the item to be re-orderable.
+     return true
+     }
+     */
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
+
+
