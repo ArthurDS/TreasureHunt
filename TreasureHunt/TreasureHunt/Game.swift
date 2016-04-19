10199ea873cf86310d2b0400c609a7fe9c502f72
Kristof Renotte <renottekristof@gmail.com>
2016-04-19 11:41:47 +0200

    cat animation

--- TreasureHunt/TreasureHunt/Game.swift
+++ TreasureHunt/TreasureHunt/Game.swift
@@ -6,20 +6,20 @@
 //
 //
 
-import UIKit
-
-class Game: NSObject {
-
-}
-
-class Riddles {
-    
-    
-}
-class Answer: NSObject {
-    
-    let description: String
-    let check: Bool
-    
-    init(description: String, check: Bool)
-}
+//import UIKit
+//
+//class Game: NSObject {
+//
+//}
+//
+//class Riddles {
+//    
+//    
+//}
+//class Answer: NSObject {
+//    
+//    let description: String
+//    let check: Bool
+//    
+//    init(description: String, check: Bool)
+//}
--- TreasureHunt/TreasureHunt/StartGameViewController.swift
+++ TreasureHunt/TreasureHunt/StartGameViewController.swift
@@ -9,12 +9,42 @@
 import UIKit
 
 class StartGameViewController: UIViewController {
+    
+    @IBOutlet weak var backgroundImage: UIImageView!
+
+    @IBOutlet weak var catAnimation: UIImageView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        var catAnimationArray = ["giphy-0", "giphy-1", "giphy-2", "giphy-3", "giphy-4", "giphy-5", "giphy-6", "giphy-7", "giphy-8", "giphy-9", "giphy-10", "giphy-11", "giphy-12"]
+        
+        var images = [UIImage]()
 
-        // Do any additional setup after loading the view.
+        for i in 0..<catAnimationArray.count {
+            images.append(UIImage(named: catAnimationArray[i])!)
+        }
+        
+        catAnimation.animationImages = images
+        catAnimation.animationDuration = 1.0
+        catAnimation.startAnimating()
+        
+
+
     }
+    
+    override func viewDidAppear(animated: Bool) {
+        
+
+        UIView.animateWithDuration(50.9, delay: 0.0, options: UIViewAnimationOptions.CurveLinear, animations: {
+            self.backgroundImage.frame.origin.x -= 350
+            }, completion: { (finished: Bool) in
+                print("Animation Ended!")
+        });
+        }
+  
+   
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
