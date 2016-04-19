ac8e58bad56df3a8ef8d998070ad360685105168
Arthur Despiegelaere <arthurdespiegelaere@gmail.com>
2016-04-19 09:18:11 +0200

    NewStoryBoard

---
+++ TreasureHunt/Game.swift
@@ -0,0 +1,23 @@
+//
+//  Game.swift
+//  
+//
+//  Created by Jean Smits on 19/04/16.
+//
+//
+
+import UIKit
+
+class Game: NSObject {
+
+}
+
+class Riddles {
+    
+    
+}
+class Answer: NSObject {
+    
+    let description: String
+    let check: 
+}
