0f45c0b0798ecb93c991dd7795632569e7c3dff5
Arthur Despiegelaere <arthurdespiegelaere@gmail.com>
2016-04-20 15:57:44 +0200

    jiji

--- TreasureHunt/TreasureHunt/PlayGameSolutionViewController.swift
+++ TreasureHunt/TreasureHunt/PlayGameSolutionViewController.swift
@@ -34,7 +34,7 @@ class PlayGameSolutionViewController: UIViewController,CLLocationManagerDelegate
     let image = UIImage(named: "sherlockmini")
     
     var clock = NSTimer()
-    var timer = 2
+    var timer = 60
     let kAnimationKey = "rotation"
 
     
@@ -47,7 +47,8 @@ class PlayGameSolutionViewController: UIViewController,CLLocationManagerDelegate
         context = CIContext(options: nil)
         currentFilter = CIFilter(name: "CISepiaTone")
         navigationItem.titleView = UIImageView(image: image)
-
+        
+        fillTheLabels()
         createButtons()
         rotateClock()
         makePictureOld()
