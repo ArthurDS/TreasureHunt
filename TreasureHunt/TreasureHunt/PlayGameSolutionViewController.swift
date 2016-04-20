e7bbefedc5693189ace242b9f9a971239d7ad52c
Arthur Despiegelaere <arthurdespiegelaere@gmail.com>
2016-04-20 10:37:16 +0200

    ,k,k,k

--- TreasureHunt/TreasureHunt/PlayGameSolutionViewController.swift
+++ TreasureHunt/TreasureHunt/PlayGameSolutionViewController.swift
@@ -1,6 +1,6 @@
 //
 //  PlayGameSolutionViewController.swift
-//  
+//
 //
 //  Created by Jean Smits on 19/04/16.
 //
@@ -9,28 +9,31 @@
 import UIKit
 
 class PlayGameSolutionViewController: UIViewController {
-
+    
     @IBOutlet weak var locationImageView: UIImageView!
-
+    
     @IBOutlet weak var answerButton1: UIButton!
     
     @IBOutlet weak var timerLabel: UILabel!
- 
+    
     @IBOutlet weak var answerButton2: UIButton!
     
     @IBOutlet weak var answerButton3: UIButton!
     
     @IBOutlet weak var answerButton4: UIButton!
     
+    var timer = 10
     
-    var timer = 60
+    var clock = NSTimer()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        var clock = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: #selector(PlayGameSolutionViewController.countdown), userInfo: nil, repeats: true)
-
-fillTheLabels()
+        
+        clock = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: #selector(PlayGameSolutionViewController.countdown), userInfo: nil, repeats: true)
+        
+        fillTheLabels()
+        
         answerButton1.layer.cornerRadius = 20
         answerButton1.layer.borderWidth = 2
         answerButton1.layer.borderColor = UIColor.blackColor().CGColor
@@ -48,10 +51,10 @@ fillTheLabels()
         answerButton4.layer.borderColor = UIColor.blackColor().CGColor
         
         fillTheLabels()
-    
+        
         let image = UIImage(named: "sherlockmini")
         navigationItem.titleView = UIImageView(image: image)
-
+        
         // Do any additional setup after loading the view.
     }
     
@@ -61,27 +64,41 @@ fillTheLabels()
     }
     
     func fillTheLabels() {
-    
+        
         
     }
     
     func countdown() {
-        timerLabel.text = String(timer)
-        timer -= 1
-        
-        if timer <= 0 {
-            timerLabel.text = "Verloren"
-        
+        if timer > 0 {
+            timerLabel.text = String(timer)
+            timer -= 1
+            
+        }
+        else {
+            
+            clock.invalidate()
+            timesupAlert()
+            
         }
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    func timesupAlert() {
+        let alert: UIAlertView = UIAlertView()
+        alert.title = "Watson:"
+        alert.message = "Unfortunately, the time is up sir"
+        alert.delegate = self
+        alert.addButtonWithTitle("Shut up WaRson")
+        alert.show()
+        
     }
-    */
-
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
