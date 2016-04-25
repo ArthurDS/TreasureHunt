b8bf96c0ebbd308acd8070414e6f619c4f19d0af
Arthur Despiegelaere <arthurdespiegelaere@gmail.com>
2016-04-25 13:25:22 +0200

    buttons hidden

---
+++ TreasureHunt/TreasureHunt/CluesViewController.swift
@@ -0,0 +1,73 @@
+//
+//  CluesViewController.swift
+//  TreasureHunt
+//
+//  Created by Jean Smits on 25/04/16.
+//  Copyright © 2016 Embur. All rights reserved.
+//
+
+import UIKit
+
+class CluesViewController: UIViewController {
+
+    @IBOutlet weak var answerButton1: UIButton!
+    @IBOutlet weak var answerButton2: UIButton!
+    @IBOutlet weak var answerButton3: UIButton!
+    @IBOutlet weak var answerButton4: UIButton!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        hideTheButtons()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    func hideTheButtons() {
+        answerButton1.hidden = true
+        answerButton1.userInteractionEnabled = false
+        
+        answerButton2.hidden = true
+        answerButton2.userInteractionEnabled = false
+
+        answerButton3.hidden = true
+        answerButton3.userInteractionEnabled = false
+
+        answerButton4.hidden = true
+        answerButton4.userInteractionEnabled = false
+
+    }
+    
+    func ShowTheButtons() {
+        
+        answerButton1.hidden = false
+        answerButton1.userInteractionEnabled = true
+        
+        answerButton2.hidden = false
+        answerButton2.userInteractionEnabled = true
+        
+        answerButton3.hidden = false
+        answerButton3.userInteractionEnabled = true
+        
+        answerButton4.hidden = false
+        answerButton4.userInteractionEnabled = true
+    }
+    
+    
+    
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
